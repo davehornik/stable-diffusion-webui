3dc37a44fe3049d1c0d5f0d97e524c45dba1a250
Dave <horna96@seznam.cz>
2023-05-14 01:30:42 +0200

    Added czech language, wip

--- webui-user.bat
+++ webui-user.bat
@@ -1,8 +1,8 @@
 @echo off
 
-set PYTHON=
-set GIT=
-set VENV_DIR=
-set COMMANDLINE_ARGS=
+set PYTHON="C:\Users\david\AppData\Local\Programs\Python\Python310\python.exe"
+set VENV_DIR=D:\Stable WEBUI\stable-diffusion-webui\venv
+set COMMANDLINE_ARGS= --autolaunch --xformers --ckpt-dir "D:\Stable WEBUI\stable-diffusion-webui\models\Stable-diffusion" --vae-dir "D:\Stable WEBUI\stable-diffusion-webui\models\VAE" --lora-dir "D:\Stable WEBUI\stable-diffusion-webui\models\Lora" --embeddings-dir "D:\Stable WEBUI\stable-diffusion-webui\embeddings"
 
+git pull dev
 call webui.bat
